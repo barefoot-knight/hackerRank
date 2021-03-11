00675275e20dc7386e593e317742e45e1f72e2b5
barefoot-knight <barefoot.knight@pm.me>
2021-03-10 21:53:14 -0600

    Refactored ifElse.py

--- ifElse.py
+++ ifElse.py
@@ -1,14 +1,26 @@
+### HackerRank Python If-Else
+### barefoot.knight
+### 03/10/2021
+
+# Checks if user entered a numberical integer
 try : 
-    integer = input()
+    integer = input('Please enter an integer: ')
     integer = int(integer) 
 except :
     print('Please enter a numerical integer.')
 
-elif integer % 2 != 0:
+# Tests if even or odd
+if integer % 2 != 0:
     print('Weird')
+
+# Tests if between 2 and 5
 elif integer >= 2 and integer <= 5:
     print('Not Weird')
+
+# Tests if between 6 and 20
 elif integer >= 6 and integer <= 20:
     print('Weird')
+
+#  Tests if over 20
 elif integer > 20: 
     print('Not Weird')
